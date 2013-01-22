6a627eb06a8927a50342e292de102f3fe01e5c08
Aeon-Linn <aeonlinn@gmail.com>
2013-01-21 18:28:23 -0800

    Create chinese_trad.js
    中文(繁體)

---
+++ js/translations/chinese_trad.js
@@ -0,0 +1,184 @@
+ui.language({
+  //List Headers
+	focus: "重點",
+	lists: "清單",
+	today: "今天",
+	next: "下一個",
+	scheduled: "時程表",
+	logbook: "紀錄簿",
+	all: "全部任務",
+
+	// Empty List Filler Text
+	noTasksInList: "在 %s 無任務清單",
+	thisList: "本清單",
+
+	// Search Result Header
+	searchResults: "搜尋結果",
+
+	//Default Names
+	nlist: "新清單",
+	ntask: "新任務",
+
+	//Buttons
+	addbtn: "新增",
+	deletebtn: "刪除",
+	sortbtn: "排序",
+	addList: "新增清單",
+	moveToLogbookSingle: "移動 1 項已完成的任務至紀錄簿",
+	moveToLogbookPlural: "移動 %s 項已完成的任務至紀錄簿",
+	showLoggedTask: "顯示 1 筆紀錄的任務",
+	showLoggedTasks: "顯示 %s 筆紀錄的任務",
+
+	// Sort
+	sortMagic: "智慧",
+	sortDefault: "手動",
+	sortPriority: "優先",
+	sortDate: "日期",
+	sortTitle: "標題",
+
+	//Placeholder Text
+	search: "搜索",
+	notes: "註記",
+	tags: "標籤, 以逗號區隔",
+
+	//Priority
+	none: "無",
+	low: "低",
+	medium: "中等",
+	high: "重要",
+
+	//Date Labels
+	dueDate: "到期日期",
+	schedule: "時程表",
+
+	// Due Date Labels
+	daysOverdue: "已逾期 %s 天",
+	dueYesterday: "昨天到期",
+	dueToday: "今天到期",
+	dueTomorrow: "明天到期",
+	daysLeft: "還剩 %s 天",
+
+	// Datepicker
+	weekStartsOn: "週開始於：",
+
+	// Days and Months
+	sunday: "週日",
+	monday: "週一",
+	tuesday: "週二",
+	wednesday: "週三",
+	thursday: "週四",
+	friday: "週五",
+	saturday: "週六",
+
+	sunShort: "日",
+	monShort: "一",
+	tueShort: "二",
+	wedShort: "三",
+	thuShort: "四",
+	friShort: "五",
+	satShort: "六",
+
+	sunMin: "日",
+	monMin: "一",
+	tueMin: "二",
+	wedMin: "三",
+	thuMin: "四",
+	friMin: "五",
+	satMin: "六",
+
+	january: "一月",
+	february: "二月",
+	march: "三月",
+	april: "四月",
+	may: "五月",
+	june: "六月",
+	july: "七月",
+	august: "八月",
+	september: "九月",
+	october: "十月",
+	november: "十一月",
+	december: "十二月",
+
+	janShort: "一月",
+	febShort: "二月",
+	marShort: "三月",
+	aprShort: "四月",
+	mayShort: "五月",
+	junShort: "六月",
+	julShort: "七月",
+	augShort: "八月",
+	sepShort: "九月",
+	octShort: "十月",
+	novShort: "十一月",
+	decShort: "十二月",
+
+	// Delete Modals
+	deleteMore: "您確定要刪除這 %s 項任務嗎？",
+	deleteMoreYes: "是，刪除",
+	deleteMoreNo: "不，保留",
+	deleteOne: "您確定要刪除該任務嗎？",
+	deleteOneYes: "是，刪除",
+	deleteOneNo: "不，保留",
+	deleteList: "您確定要刪除該清單嗎？",
+	warning: "警告",
+
+	recurring: "循環",
+	reviewNo: "複閱：",
+	recurType: "創建：",
+	daily: "每日",
+	weekly: "每周",
+	monthly: "每月",
+	recurNext: "接下來：",
+	recurEnds: "結束：",
+	reviewAction: "且放在: ",
+	create: "創建",
+	cancel: "取消",
+
+	hideWarnings: "隱藏刪除警告",
+	nextNoLists: "不在「下一個」中顯示任何任務",
+	nextEverything:	"顯示全部任務",
+
+	general: "一般",
+	nextDescription: "在「下一個」中顯示其他清單：", 
+	deleteWarningsDescription: "當您刪除任務或清單時這會停用確認視窗。",
+
+	//Clear Data
+	resetnitro: "Reset Nitro: ",
+	cleardata: "Clear Data",
+	clearDataMsg: "確定要重設 Nitro? 長期以來的所有資料會永久清除",
+
+	language: "語言",
+	authortext: "作者",
+
+	about: "關於",
+	theme: "主題",
+	pickTheme: "選取一款主題：",
+	themeDescription: "主題允許您更改 Nitro 的外觀。您甚至可以創建您自己的主題！",
+	replaceDefault:"更改背景：",
+	useDefault: "使用預設背景：",
+	removeBG: "移除背景",
+	bgSize: "背景尺寸：",
+	fill:"填滿螢幕",
+	shrink:	"縮小",
+	tile:"並列",
+	bgDescription: "更改顏色讓外觀看起來與自定義背景協調。",
+	headingColor: "標題顏色",
+	default: "預設",
+	light: "淺色",
+	dark:"黑色",
+	headingDescription:	"更改樣板顏色讓外觀看起來與自定義背景協調。",
+	sync: "同步",
+	donateText:	"同步功能處於 Beta 階段。如果 Nitro 壞掉，它會把您的電腦整個底朝天 - 別說沒告訴你。它很穩定的，但如果有任何事情發生，您可以聯絡<a href='https://twitter.com/GeorgeCzabania'>@GeorgeCzabania</a> 或 <a href='https://twitter.com/consindo'>@consindo",
+	syncLabel: "運行同步：",
+	syncDescription:"同步允許您從其他電腦或智慧手機上查詢任務。",
+	syncNever: "從不(不通知)",
+	syncManual:	"手動",
+	syncTimer: "每 30 秒鐘",
+	syncAuto: "每次都做更改",
+	syncLoggedIn: "登錄名：",
+	syncService:"服務：",
+	syncLogout: "登出",
+	syncAuthenticate: "等待授權 Nitro...",
+	syncError: "無法與伺服器器同步...",
+	cancel: "取消"
+})
